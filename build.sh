b224e1dc26252e2959029970ae375b94e9a56bec
Luan Truong <truongluan303@gmail.com>
2024-01-02 15:21:09 -0800

    Fix build script

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@ build_dir="build"
 mode="BUILD_ONLY"
 
 if [ "$1" ]; then
-    if [ "$1" == "test" ]; then
+    if [ "$1" = "test" ]; then
         mode="TEST";
-    elif [ "$1" == "run" ]; then
+    elif [ "$1" = "run" ]; then
         mode="RUN";
     else
         echo "ERROR: Unknown option!";
@@ -19,11 +19,11 @@ if [ $? -ne 0 ]; then
     exit 1;
 fi
 
-if [ "$mode" == "BUILD_ONLY" ]; then
+if [ "$mode" = "BUILD_ONLY" ]; then
     exit 0;
-elif [ "$mode" == "TEST" ]; then
+elif [ "$mode" = "TEST" ]; then
     ./test/TestSort;
-elif [ "$mode" == "RUN" ]; then
+elif [ "$mode" = "RUN" ]; then
     ./src/Main;
 fi
 
