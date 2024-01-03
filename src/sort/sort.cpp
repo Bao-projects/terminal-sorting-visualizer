3a68641075267c4c0dec95fce681c7578541ed81
Bao Than <bao.v.than@gmail.com>
2024-01-02 22:31:04 -0800

    implement bubblesort algo

--- src/sort/sort.cpp
+++ src/sort/sort.cpp
@@ -1,10 +1,21 @@
 #include "sort.hpp"
 
 #include <algorithm> // TODO: remove this when all algos are implemented.
+#define ARR_LEN arr.size()
 
-
+/**
+ * @brief Bubble Sort.
+ *
+ * @param arr
+ */
 void bubble_sort(std::vector<uint16_t>& arr) {
-    std::sort(arr.begin(), arr.end()); // TODO: remove this and actually implement the algo.
+    for(int i = 0; i < ARR_LEN -1; i++){
+        for(int j = 0; j < ARR_LEN - i -1; j ++){
+            if(arr[j] > arr[j + 1]){
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
 }
 
 void selection_sort(std::vector<uint16_t>& arr) {
